686fea484b3b6525aefa179ae2b86bb4adc16e58
jcd-formateur <jcd.pro+formateur@gmail.com>
2022-06-28 09:34:07 +0200

    init projet avec Starter SpringBoot

---
+++ src/main/java/com/m2i/apifilrougecrm/ApiFilRougeCrmApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.apifilrougecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilRougeCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFilRougeCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m2i/apifilrougecrm/ApiFilRougeCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.apifilrougecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilRougeCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
